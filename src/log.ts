16536d5ec314494c0e1e703ab9635742b8c3a807
Fabian Pecher <fabian.pecher@tngtech.com>
2020-05-22 10:51:44 +0200

    VOS#6: Add pino & lodash and pin dependencies via package-lock.json

---
+++ src/log.ts
@@ -0,0 +1,26 @@
+import pino from "pino";
+
+type LogFn = (msg: string, obj?: any) => void;
+export interface Logger {
+    trace: LogFn;
+    debug: LogFn;
+    info: LogFn;
+    warn: LogFn;
+    error: LogFn;
+}
+
+const instance = pino({
+    prettyPrint: true,
+});
+
+function buildLogFn(level: string) {
+    return (message: string, data?: any) => instance[level](data || {}, message);
+}
+
+export const logger: Logger = {
+    trace: buildLogFn("trace"),
+    debug: buildLogFn("debug"),
+    info: buildLogFn("info"),
+    warn: buildLogFn("warn"),
+    error: buildLogFn("error"),
+};
