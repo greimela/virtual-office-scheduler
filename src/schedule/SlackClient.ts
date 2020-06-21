e9780216d519a23d8844e26d4146a08dba1e184e
Andreas Greimel <andreas.greimel@tngtech.com>
2020-06-21 21:29:31 +0200

    Extract SlackClient to separate file

---
+++ src/schedule/SlackClient.ts
@@ -0,0 +1,67 @@
+import { WebClient } from "@slack/web-api";
+import { logger } from "../log";
+
+interface Channel {
+  id: string;
+  name: string;
+}
+
+export class SlackClient {
+  private client: WebClient;
+  private enableRateLimiting: boolean;
+  private lastRequestTimestamps: { [method: string]: number } = {};
+
+  constructor(config: { SLACK_ENABLE_RATE_LIMITING?: "true" | "false"; SLACK_TOKEN: string }) {
+    this.client = new WebClient(config.SLACK_TOKEN);
+    this.enableRateLimiting = config.SLACK_ENABLE_RATE_LIMITING === "true";
+  }
+
+  async getAllChannels(): Promise<Channel[]> {
+    const channels: Channel[] = [];
+
+    let cursor;
+    do {
+      logger.info(`Fetching Slack channels${cursor ? ` for cursor ${cursor}` : ""}`);
+      const result = await this.client.conversations.list({
+        limit: 1000,
+        types: "public_channel",
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        exclude_archived: true,
+        cursor,
+      });
+      if (result.ok) {
+        cursor = result.response_metadata?.next_cursor;
+        channels.push(...(result.channels as Channel[]));
+      } else {
+        throw new Error(result.error);
+      }
+    } while (cursor);
+
+    return channels;
+  }
+
+  async createChannelIfNotExists(options: { name: string }): Promise<boolean> {
+    await this.limitRate("conversations.create", 1000 * 3);
+
+    try {
+      const result = await this.client.conversations.create(options);
+      return result.ok;
+    } catch (e) {
+      const error = e?.data?.error;
+      if (error === "name_taken") {
+        return false;
+      }
+      throw e;
+    }
+  }
+
+  private async limitRate(functionName: string, millisBetweenRequests: number): Promise<void> {
+    if (this.enableRateLimiting && this.lastRequestTimestamps[functionName]) {
+      const millisSinceLastRequest = Date.now() - this.lastRequestTimestamps[functionName];
+      if (millisBetweenRequests - millisSinceLastRequest > 0) {
+        await new Promise((resolve) => setTimeout(resolve, millisBetweenRequests - millisSinceLastRequest));
+      }
+    }
+    this.lastRequestTimestamps[functionName] = Date.now();
+  }
+}
--- src/schedule/createSlackChannels.ts
+++ src/schedule/createSlackChannels.ts
@@ -1,72 +1,8 @@
-import { WebClient } from "@slack/web-api";
 import { Office, Room, RoomLink } from "./generateOffice";
 import { ScheduleEnvironment } from "../config";
 import { logger } from "../log";
 import { iconUrlFor } from "./extractLinks";
-
-interface Channel {
-  id: string;
-  name: string;
-}
-
-class SlackClient {
-  private client: WebClient;
-  private enableRateLimiting: boolean;
-  private lastRequestTimestamps: { [method: string]: number } = {};
-
-  constructor(config: { SLACK_ENABLE_RATE_LIMITING?: "true" | "false"; SLACK_TOKEN: string }) {
-    this.client = new WebClient(config.SLACK_TOKEN);
-    this.enableRateLimiting = config.SLACK_ENABLE_RATE_LIMITING === "true";
-  }
-
-  async getAllChannels(): Promise<Channel[]> {
-    const channels: Channel[] = [];
-
-    let cursor;
-    do {
-      logger.info(`Fetching Slack channels${cursor ? ` for cursor ${cursor}` : ""}`);
-      const result = await this.client.conversations.list({
-        limit: 1000,
-        types: "public_channel",
-        // eslint-disable-next-line @typescript-eslint/camelcase
-        exclude_archived: true,
-        cursor,
-      });
-      if (result.ok) {
-        cursor = result.response_metadata?.next_cursor;
-        channels.push(...(result.channels as Channel[]));
-      } else {
-        throw new Error(result.error);
-      }
-    } while (cursor);
-
-    return channels;
-  }
-
-  async createChannelIfNotExists(options: { name: string }): Promise<boolean> {
-    await this.limitRate("conversations.create", 1000 * 3);
-    try {
-      const result = await this.client.conversations.create(options);
-      return result.ok;
-    } catch (e) {
-      const error = e?.data?.error;
-      if (error === "name_taken") {
-        return false;
-      }
-      throw e;
-    }
-  }
-
-  private async limitRate(functionName: string, millisBetweenRequests: number): Promise<void> {
-    if (this.enableRateLimiting && this.lastRequestTimestamps[functionName]) {
-      const millisSinceLastRequest = Date.now() - this.lastRequestTimestamps[functionName];
-      if (millisBetweenRequests - millisSinceLastRequest > 0) {
-        await new Promise((resolve) => setTimeout(resolve, millisBetweenRequests - millisSinceLastRequest));
-      }
-    }
-    this.lastRequestTimestamps[functionName] = Date.now();
-  }
-}
+import { SlackClient } from "./SlackClient";
 
 function getChannelLink(baseUrl, channelName: string): RoomLink {
   const href = `${baseUrl}/app_redirect?channel=${channelName}`;
