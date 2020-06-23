20e7614da8fcad26b221019c4952e06d1c3ec053
Andreas Greimel <andreas.greimel@tngtech.com>
2020-06-23 15:32:37 +0200

    Get host key for each zoom user and store in Spreadsheet

--- src/createMeetings/uploadToSpreadsheet.ts
+++ src/createMeetings/uploadToSpreadsheet.ts
@@ -14,10 +14,10 @@ export async function uploadToSpreadsheet(
     throw new Error(`cannot find meetings sheet (${config.MEETINGS_SHEET_NAME})`);
   }
   await sheet.clear();
-  sheet.headerValues = ["email", "meetingId", "joinUrl"];
-  await sheet.addRow({ email: "email", meetingId: "meetingId", joinUrl: "joinUrl" });
+  sheet.headerValues = ["email", "meetingId", "joinUrl", "hostKey"];
+  await sheet.addRow({ email: "email", meetingId: "meetingId", joinUrl: "joinUrl", hostKey: "hostKey" });
 
   for (const { user, meeting } of meetings) {
-    await sheet.addRow({ email: user.email, meetingId: meeting.id, joinUrl: meeting.join_url });
+    await sheet.addRow({ email: user.email, meetingId: meeting.id, joinUrl: meeting.join_url, hostKey: user.host_key });
   }
 }
