d56dd522d4a511f5989a99b1b280bf6789eff52c
Andreas Schmid <service@aaschmid.de>
2020-06-30 16:56:55 +0200

    refactor confluence page creation for readability (#30)
    Signed-off-by: Andreas Schmid <service@aaschmid.de>

--- src/schedule/createConfluencePages.ts
+++ src/schedule/createConfluencePages.ts
@@ -1,33 +1,19 @@
-import { hostKey, Office } from "./generateOffice";
+import { hostKey, Office, Room } from "./generateOffice";
 import { logger } from "../log";
 import { iconUrlFor } from "./extractLinks";
 import { ConfluenceClient, ConfluenceConfig } from "./ConfluenceClient";
 
 export async function createConfluencePagesAndInsertLinks(office: Office, config: ConfluenceConfig): Promise<Office> {
-  const confluenceClient = new ConfluenceClient(config);
-  const templateStorageContent = await confluenceClient.getPageBody(config.CONFLUENCE_TEMPLATE_PAGE_ID);
+  const client = new ConfluenceClient(config);
+  const templateStorageContent = await client.getPageBody(config.CONFLUENCE_TEMPLATE_PAGE_ID);
 
-  const confluencePages = office.rooms
-    .filter((room) => room.hasConfluencePage)
-    .map((room) => ({ room, pageTitle: `vSR20 - Session ${room.name}` }));
-  for (const { room, pageTitle } of confluencePages) {
-    let pageLink = await confluenceClient.findLinkForPage(config.CONFLUENCE_SPACE_KEY, pageTitle);
-
-    if (pageLink === undefined) {
-      const linkList = room.links.map((link) => {
-        if (link.text.startsWith(hostKey)) {
-          return `<li><a href="${room.joinUrl}">Zoom</a> (<a href="${link.href}">${link.text}</a>)</li>`;
-        }
-        return `<li><a href="${link.href}">${link.text}</a></li>`;
-      });
-      const content = templateStorageContent.replace("$LINKS", `<ul>${linkList}</ul>`);
-
-      pageLink = await confluenceClient.createSessionPage(pageTitle, content);
-      logger.info(`Created Confluence page ${pageTitle}`);
-    } else {
-      logger.info(`Confluence page ${pageTitle} already exists`);
+  const confluencePages = office.rooms.filter((room) => room.hasConfluencePage);
+  for (const room of confluencePages) {
+    if (!room.hasConfluencePage) {
+      continue;
     }
 
+    const pageLink = await getOrCreateConfluencePage(client, config.CONFLUENCE_SPACE_KEY, room, templateStorageContent);
     room.links.push({
       text: "Confluence",
       href: pageLink,
@@ -35,4 +21,31 @@ export async function createConfluencePagesAndInsertLinks(office: Office, config
     });
   }
   return office;
+}
+
+async function getOrCreateConfluencePage(
+  client: ConfluenceClient,
+  spaceKey: string,
+  room: Room,
+  templateStorageContent: string
+): Promise<string> {
+  const pageTitle = `vSR20 - Session ${room.name}`;
+
+  const pageLink = await client.findLinkForPage(spaceKey, pageTitle);
+  if (pageLink) {
+    logger.info(`Confluence page '${pageTitle}' already exists`);
+    return pageLink;
+  }
+
+  const linkList = room.links.map((link) => {
+    if (link.text.startsWith(hostKey)) {
+      return `<li><a href="${room.joinUrl}">Zoom</a> (<a href="${link.href}">${link.text}</a>)</li>`;
+    }
+    return `<li><a href="${link.href}">${link.text}</a></li>`;
+  });
+  const content = templateStorageContent.replace("$LINKS", `<ul>${linkList}</ul>`);
+
+  const result = await client.createSessionPage(pageTitle, content);
+  logger.info(`Created Confluence page '${pageTitle}'`);
+  return result;
 }
